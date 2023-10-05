232ec800eaab92af88d743dd5b989e357730e42e
Anumaneni Venkat Balachandra <128356330+venkat1924@users.noreply.github.com>
2023-10-05 19:51:55 +0530

    Create ldr.ino

---
+++ ldr.ino
@@ -0,0 +1,26 @@
+const int ldrpin=A0;
+const int ledpin=2;
+int ldrstatus;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ldrpin,INPUT);
+  pinMode(ledpin,OUTPUT);
+}
+
+void loop()
+{
+  ldrstatus=analogRead(ldrpin);
+  if (ldrstatus <=200)
+  {
+    digitalWrite(ledpin, LOW);
+    Serial.print("It's BRIGHT, turn off the LED");
+    Serial.println(ldrstatus);
+  }
+  else
+  {
+    digitalWrite(ledpin, HIGH);
+    Serial.print("It's DARK, turn on the LED");
+    Serial.println(ldrstatus);
+  }
