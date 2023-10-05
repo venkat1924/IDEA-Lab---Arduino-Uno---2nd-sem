188a54e6ed7b1d3b086e5a6d907af63aae77d516
Anumaneni Venkat Balachandra <128356330+venkat1924@users.noreply.github.com>
2023-10-05 18:33:24 +0530

    go for it

---
+++ Float.ino
@@ -0,0 +1,20 @@
+int FloatSensor = 2;
+int led = 13;
+int buttonState = 1;
+void setup(){
+  Serial.begin(9600);
+  pinMode(FloatSensor, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);
+}
+void loop(){
+  buttonState = digitalRead(FloatSensor);
+  if(buttonState == HIGH){
+    digitalWrite(led, LOW);
+    Serial.println("WATERLEVEL - HIGH");
+  }else{
+    digitalWrite(led, HIGH);
+    Serial.println("WATERLEVEL - LOW");
+  }
+  delay(1000);
+}
